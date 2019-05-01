f92cf6f9610f004b425df8f0b412d1ac90dfdf67
leonardofaria00 <leonardofaria00@gmail.com>
2019-05-01 00:01:11 -0300

    [FIX] Criação do mapeamento HomeController e classes de config.

---
+++ src/main/java/br/com/sgedu/controller/HomeController.java
@@ -0,0 +1,16 @@
+package br.com.sgedu.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class HomeController {
+
+	@RequestMapping("/")
+	public ModelAndView index() {
+		System.out.println("Acessando a aplicação");
+		ModelAndView view = new ModelAndView("index");
+		return view;
+	}
+}
